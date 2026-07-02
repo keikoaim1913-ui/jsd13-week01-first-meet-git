254467aba5c7a2b3f143f0c2ee2d47a879c5c3af
keikoaim1913-ui <keiko.aim1913@gmail.com>
2026-07-02 13:37:30 +0700

    added in array object, render in html page

---
+++ week-01/first-meet-git/My-Object.js
@@ -0,0 +1,51 @@
+let foodDatabase = [
+    { id:1,
+      name:"Pizza",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+    //{name: "Mala Tang", price:"249"},
+    //{name: "Pad-Thai", price:"249"},
+    //{name: "Sushi", price:"249"},
+    //{name: "Medovik", price:"249"},
+    //{name: "Burger", price:"249"},
+
+    { id:2,
+      name:"Mala Tang",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+
+    { id:3,
+      name:"Pad-Thai",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+
+    { id:4,
+      name:"Sushi",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+
+    { id:5,
+      name:"Medovik",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+
+    { id:5,
+      name:"Burger",
+      cuisine:"Italy",
+      orijin:"Italy",
+      vegetarain:"false",
+    },
+]
+
+window.foodDatabase = foodDatabase;
+console.log(foodDatabase);
---
+++ week-01/first-meet-git/Myarray.js
@@ -0,0 +1,7 @@
+//let food = ["Pizza" , "Mala Tang", "Pad-Thai","Burger","Sushi","Medovik"];
+
+//console.log(food);
+
+const dishes = ["Pizza"]
+
+console.log(dishes)
